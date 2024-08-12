8bd34938d096056213e7c26bfaf00292707d1ddd
tanmaydesai07 <tanmay2004desai@gmail.com>
2024-08-13 00:48:10 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:testapp_booking/firebase_options.dart';
 import 'package:testapp_booking/pages/HomePage.dart';
 import 'package:testapp_booking/pages/login.dart';
 import 'package:testapp_booking/pages/mainHome.dart';
 
-void main() {
+Future<void> main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform,);
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
